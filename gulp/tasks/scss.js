d0ac02a85fb0d6bb6e9e5e4528cca10ec76b757d
imprisoner <continentalresident@yandex.ru>
2023-03-02 22:11:51 +0400

    horizontal scroll fix

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -1,8 +1,8 @@
-import gulp from "gulp"
-import dartSass from "sass"
-import gulpSass from "gulp-sass"
-import sourcemaps from "gulp-sourcemaps"
-import autoprefixer from "gulp-autoprefixer"
+import gulp from 'gulp'
+import dartSass from 'sass'
+import gulpSass from 'gulp-sass'
+import sourcemaps from 'gulp-sourcemaps'
+import autoprefixer from 'gulp-autoprefixer'
 import { GLOBS } from "../config.js"
 
 const { src, dest } = gulp
@@ -10,12 +10,13 @@ const sass = gulpSass(dartSass)
 
 export default function scss(done) {
   src(GLOBS.SCSS.SRC)
-    .pipe(sourcemaps.init())
+    // .pipe(sourcemaps.init())
     .pipe(sass({
-      includePaths: ["./node_modules"]
-    }).on("error", sass.logError))
+      includePaths: ["./node_modules"],
+      outputStyle: "compressed"
+    }).on('error', sass.logError))
     .pipe(autoprefixer())
-    .pipe(sourcemaps.write())
+    // .pipe(sourcemaps.write())
     .pipe(dest(GLOBS.SCSS.DEST))
 
   done()
