639354ba0566ece22a67952569c11ab7611b3ba5
imprisoner <continentalresident@yandex.ru>
2023-03-27 23:24:43 +0400

    new modal and animation

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -3,6 +3,37 @@ const classNames = {
   activeModal: "modal--active",
 };
 
+// export default function modal(
+//   selectors = {
+//     modal: "",
+//     trigger: "",
+//     close: "",
+//   }
+// ) {
+//   const overlay = document.querySelector(selectors.modal);
+//   const trigger = document.querySelector(selectors.trigger);
+//   const close = overlay.querySelector(selectors.close);
+
+//   console.log(overlay);
+//   trigger.addEventListener("click", (event) => {
+//     document.body.classList.add(classNames.bodyNoScroll);
+//     overlay.classList.add(classNames.activeModal);
+//   });
+
+//   overlay.addEventListener("click", (event) => {
+//     if (event.target !== overlay) return;
+
+//     overlay.classList.remove(classNames.activeModal);
+//     document.body.classList.remove(classNames.bodyNoScroll);
+//   });
+
+//   close.addEventListener("click", (event) => {
+//     overlay.classList.remove(classNames.activeModal);
+//     document.body.classList.remove(classNames.bodyNoScroll);
+//   });
+// }
+
+
 export default function modal(
   selectors = {
     modal: "",
@@ -14,20 +45,23 @@ export default function modal(
   const trigger = document.querySelector(selectors.trigger);
   const close = overlay.querySelector(selectors.close);
 
-  console.log(overlay);
   trigger.addEventListener("click", (event) => {
+    const scrollBarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
+    document.body.style.paddingRight = `${scrollBarWidth}px`;
     document.body.classList.add(classNames.bodyNoScroll);
     overlay.classList.add(classNames.activeModal);
   });
 
   overlay.addEventListener("click", (event) => {
     if (event.target !== overlay) return;
-
+    document.body.style.paddingRight = "";
     overlay.classList.remove(classNames.activeModal);
     document.body.classList.remove(classNames.bodyNoScroll);
   });
 
   close.addEventListener("click", (event) => {
+    document.body.style.paddingRight = "";
     overlay.classList.remove(classNames.activeModal);
     document.body.classList.remove(classNames.bodyNoScroll);
   });
